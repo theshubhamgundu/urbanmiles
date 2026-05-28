647005e41170f57dea4f75c6d2c03b2905a0623b
SHUBHAM <80262881+theshubhamgundu@users.noreply.github.com.>
2026-05-28 11:40:13 +0530

    ok

--- payment-constants.js
+++ payment-constants.js
@@ -1,10 +1,10 @@
 // Payment Constants
 const PAYMENT = {
   UPI_ID: '9063939760@ptsbi',
-  AMOUNT: '399',
+  AMOUNT: '299',
   MERCHANT_NAME: 'Urban Miles',
   QR_CODE_IMAGE: 'payment-qr.png', // Static QR code image
-  PLACEHOLDER_IMAGE: 'data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" width="180" height="180" viewBox="0 0 180 180"%3E%3Crect width="180" height="180" fill="%23f0f0f0"/%3E%3Ctext x="50%25" y="50%25" text-anchor="middle" dy="0.3em" font-family="Barlow, sans-serif" font-size="14" fill="%23999"%3ELoading QR Code...%3C/text%3E%3C/svg%3E',
+  PLACEHOLDER_IMAGE: 'data:image/svg+xml,%3Csvg xmlns="http://www.w3.org/2000/svg" width="180" height="180" viewBox="0 0 180 180"%3E%3Crect width="180" height="180" fill="%23f0f0f0"/%3E%3Ctext x="50%25" y="50%25" text-anchor="middle" dy="0.3em" font-family="Barlow, sans-serif" font-size="14" fill="%22999"%3ELoading QR Code...%3C/text%3E%3C/svg%3E',
   
   generateUPIString: function() {
     return 'upi://pay?pa=' + encodeURIComponent(this.UPI_ID) + 
